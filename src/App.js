ff821588ccfb97cf24cdd5856c48153c55709500
User <user@Users-Computer.local>
2017-05-31 23:15:46 -0400

    updated playlist

--- src/App.js
+++ src/App.js
@@ -6,8 +6,27 @@ import RightPane from './RightPane'
 import Player from './Player'
 import {Row, Col} from 'antd'
 import {Layout} from 'antd'
-import wolf from './playlist/wolf.mp3'
-import baboon from './playlist/baboon.mp3'
+
+import A1 from './playlist/value_of_values.mp3'
+import A2 from './playlist/simplicity_matters.mp3'
+import B1 from './playlist/phoenix_framework.mp3'
+import B2 from './playlist/evening_at_erlang.mp3'
+import C1 from './playlist/the_redux_journey.mp3'
+import C2 from './playlist/hot_reloading.mp3'
+import D1 from './playlist/state_of_vue.mp3'
+import D2 from './playlist/the_progressive_web_framework.mp3'
+import A3 from './playlist/c++_today.mp3'
+import A4 from './playlist/why_I_created_c++.mp3'
+import B3 from './playlist/power_of_simplicity.mp3'
+import B4 from './playlist/programming_languages.mp3'
+import C3 from './playlist/the_mess_we_are_in.mp3'
+import C4 from './playlist/reactive_systems.mp3'
+import D3 from './playlist/on_the_spectrum_of_abstraction.mp3'
+import D4 from './playlist/meta_languages.mp3'
+
+
+
+
 const {Header, Content, Footer} = Layout
 
 class App extends Component {
@@ -19,16 +38,16 @@ class App extends Component {
     this.handleNumberSelection = this.handleNumberSelection.bind(this)
     this.state = {
             leftPlaylist: [
-                {podcast1: "A1", speaker: "john smith", podcast2: "A2", title1: "foo", title2: "bar", src1: baboon, src2: wolf},
-                {podcast1: "B1", speaker: "mary smith", podcast2: "B2", title1: "foo", title2: "bar", src1: "/.mp3", src2: "/.mp3"},
-                {podcast1: "C1", speaker: "dan lee", podcast2: "C2", title1: "foo", title2: "bar", src1: "/.mp3", src2: "/.mp3"},
-                {podcast1: "D1", speaker: "dan lee", podcast2: "D2", title1: "foo", title2: "bar", src1: "/.mp3", src2: "/.mp3"},
+                {podcast1: "A1", podcast2: "A2", speaker: "Rich Hickey", title1: "Value of Values", title2: "Simplicity Matters", src1: A1, src2: A2},
+                {podcast1: "B1", podcast2: "B2", speaker: "Jose Valim", title1: "Phoenix Framework", title2: "Evening at Erlang", src1: B1, src2: B2},
+                {podcast1: "C1", podcast2: "C2", speaker: "Dan Abramov", title1: "Redux Journey", title2: "Hot Reloading", src1: C1, src2: C2},
+                {podcast1: "D1", podcast2: "D2", speaker: "Evan You", title1: "State of Vue", title2: "Progressive Web Framework", src1: D1, src2: D2},
             ],
             rightPlaylist: [
-                {podcast1: "A3", speaker: "david morgan", podcast2: "A4", title1: "foo", title2: "bar", src1: "/.mp3", src2: "/.mp3"},
-                {podcast1: "B3", speaker: "stanley hudson", podcast2: "B4", title1: "foo", title2: "bar", src1: "/.mp3", src2: "/.mp3"},
-                {podcast1: "C3", speaker: "stan ford", podcast2: "C4", title1: "foo", title2: "bar", src1: "/.mp3", src2: "/.mp3"},
-                {podcast1: "D3", speaker: "dan lee", podcast2: "D4", title1: "foo", title2: "bar", src1: "/.mp3", src2: "/.mp3"},
+                {podcast1: "A3", podcast2: "A4", speaker: "Bjarne Stroustrup", title1: "C++ Today", title2: "Why I created C++", src1: A3, src2: A4},
+                {podcast1: "B3", podcast2: "B4", speaker: "Alan Kay", title1: "Power of Simplicity", title2: "Programming Languages", src1: B3, src2: B4},
+                {podcast1: "C3", podcast2: "C4", speaker: "Joe Armstrong", title1: "The Mess We Are In", title2: "Reactive Systems", src1: C3, src2: C4},
+                {podcast1: "D3", podcast2: "D4", speaker: "Cheng Lou", title1: "Spectrum of Abstraction", title2: "Meta Languages", src1: D3, src2: D4},
             ],
             speaker: '',
             track: '',
@@ -62,9 +81,8 @@ class App extends Component {
     audio.src = source
     console.log(`audio source: ${audio.src}`)
     audio.addEventListener('loadeddata', () => {
-    console.log("Playing " + audio.src + ", for: " + audio.duration + "seconds. from: " + audio.currentSrc);
-    this.setState({file: audio, duration: audio.duration, url: audio.src})
-    //audio.play(); 
+      console.log("Playing " + audio.src + ", for: " + audio.duration + "seconds. from: " + audio.currentSrc);
+      this.setState({file: audio, duration: audio.duration, url: audio.src})
     });
     this.state.file.load()
     this.state.file.play()
@@ -77,12 +95,12 @@ class App extends Component {
     let pl = this.state.leftPlaylist.concat(this.state.rightPlaylist)
     pl.forEach(el => {
       if(sel === el.podcast1) {
-        this.setState({src1: el.src1, speaker: el.speaker, track: el.podcast1})
+        this.setState({src1: el.src1, speaker: el.speaker, track: el.title1})
       }
     })
     pl.forEach(el => {
       if(sel === el.podcast2) {
-        this.setState({src2: el.src2, speaker: el.speaker, track: el.podcast2})
+        this.setState({src2: el.src2, speaker: el.speaker, track: el.title2})
       }
     })
     this.setState({hasSelected: true, isPlaying: true})
